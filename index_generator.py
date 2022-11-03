5c502c49ce74c7a5d36a40db5610fe5f34448fac
Wilbert van Bakel <wilbert.vanbakel@gmail.com>
2022-11-03 09:19:09 -0500

    Add files via upload

---
+++ index_generator.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python3
+"""Process directory or list
+"""
+
+import os
+from bread_crumbs import HTML_FOOTER, HTML_HEADER, ignore_items, TITLES_TXT
+from generate_table_row import generate_table_row
+
+
+def process_directory():
+    """Process the current directory
+    """
+    with os.scandir() as dir_item:
+        dir_list = []
+        file_list = []
+
+    for entry in dir_item:
+        if not entry.name.startswith('.') and entry.name not in ignore_items:
+            if entry.is_dir():
+                dir_list.append(entry.name)
+            if entry.is_file():
+                file_list.append(entry.name)
+
+    dir_list = sorted(dir_list)
+    for item in dir_list:
+        generate_table_row(item, f'{item} /')
+
+    file_list = sorted(file_list)
+    for item in file_list:
+        item_basename, item_type = os.path.splitext(item)
+        generate_table_row(f'{item_basename}{item_type}', item_basename)
+
+
+def process_list(titles_txt):
+    """Argument: name of text file with urls and subjects
+    """
+
+    colon_pos = 0
+    line = ''
+    lines = ['']
+
+    with open(titles_txt, encoding="utf-8") as f_handle:
+        lines = f_handle.readlines()
+    for line in lines:
+        colon_pos = line.find(':')
+        filename = line[:colon_pos]
+        subject = line[colon_pos + 1:].strip()
+        generate_table_row(filename, subject)
+
+
+if __name__ == '__main__':
+    print(f'{HTML_HEADER}')
+    if os.path.exists(TITLES_TXT):
+        process_list(TITLES_TXT)
+    else:
+        process_directory()
+    print(f'{HTML_FOOTER}')
